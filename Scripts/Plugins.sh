db95bf50d4c885d6ff97cdf7545b8fc31eed338e
herofence <113574064+herofence@users.noreply.github.com>
2023-06-23 15:31:40 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -5,11 +5,11 @@
 #git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 #git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Argon Theme
-#git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
-#git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git
+git clone --depth=1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git
 #Design Theme
-#git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
-#git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Passwall
 git clone --depth=1 --single-branch --branch "luci" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci
 git clone --depth=1 --single-branch --branch "packages" https://github.com/xiaorouji/openwrt-passwall.git ./pw_packages
