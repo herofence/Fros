69996611f104e2ae2a5238279e00c82e68a0bd9c
herofence <113574064+herofence@users.noreply.github.com>
2023-07-28 19:27:14 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -3,22 +3,20 @@
 #luci
 git clone --depth=1 --single-branch https://github.com/openwrt/luci.git
 #Linkease
-#git clone --depth=1 --single-branch https://github.com/linkease/istore.git
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
-#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
+git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Design Theme
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
-git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+# git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+# git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
+# git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
+# git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
 #Passwall
 git clone --depth=1 --single-branch --branch "luci" https://github.com/xiaorouji/openwrt-passwall.git ./pw_luci
 git clone --depth=1 --single-branch --branch "packages" https://github.com/xiaorouji/openwrt-passwall.git ./pw_packages
 #Diskman
-#git clone --depth=1 --single-branch https://github.com/lisaac/luci-app-diskman.git
+git clone --depth=1 --single-branch https://github.com/lisaac/luci-app-diskman.git
 #docker
-#git clone --depth=1 --single-branch https://github.com/lisaac/luci-lib-docker.git
-#git clone --depth=1 --single-branch https://github.com/lisaac/luci-app-dockerman.git
-#Fros
-git clone --depth=1 --single-branch https://github.com/openfros/fros.git
+git clone --depth=1 --single-branch https://github.com/lisaac/luci-lib-docker.git
+git clone --depth=1 --single-branch https://github.com/lisaac/luci-app-dockerman.git
