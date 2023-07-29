337429ffb339ee2f706cd0998e5463a024237a19
herofence <113574064+herofence@users.noreply.github.com>
2023-07-30 07:09:08 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
+删除冲突的插件
+rm -rf ./build_dir/target-x86_64_musl/root-x86/etc/hotplug.d/ntp/25-dnsmasqsec
+rm -rf ./build_dir/target-x86_64_musl/root-x86/etc/init.d/dnsmasq
+rm -rf ./build_dir/target-x86_64_musl/root-x86/etc/uci-defaults/50-dnsmasq-migrate-ipset.sh
+rm -rf ./build_dir/target-x86_64_musl/root-x86/etc/uci-defaults/50-dnsmasq-migrate-resolv-conf-auto.sh
+rm -rf ./build_dir/target-x86_64_musl/root-x86/usr/lib/dnsmasq/dhcp-script.sh
+rm -rf ./build_dir/target-x86_64_musl/root-x86/usr/sbin/dnsmasq
+rm -rf ./build_dir/target-x86_64_musl/root-x86/usr/share/acl.d/dnsmasq_acl.json
+rm -rf ./build_dir/target-x86_64_musl/root-x86/usr/share/dnsmasq/dhcpbogushostname.conf
+rm -rf ./build_dir/target-x86_64_musl/root-x86/usr/share/dnsmasq/rfc6761.conf
 #删除冲突插件
-# rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
 #修改默认主题
-# sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改默认IP地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
 #修改默认主机名
