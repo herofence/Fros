bd158731ab2d430aa54a690407a2b8a266d2a4b8
herofence <113574064+herofence@users.noreply.github.com>
2023-07-31 18:59:51 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Linkease
-git clone --depth=1 --single-branch https://github.com/linkease/istore.git
-git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
-git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
+#git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+#git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Design Theme
-git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
-git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
+#git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
