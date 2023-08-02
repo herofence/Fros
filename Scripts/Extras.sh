02ef7d6b42d1890758359d706eb7b061a2668a1c
herofence <113574064+herofence@users.noreply.github.com>
2023-08-02 19:41:46 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -2,7 +2,7 @@
 
 #增加主题
 echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
-#echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 #echo "CONFIG_PACKAGE_luci-app-design-config=y" >> .config
 #增加luci界面
