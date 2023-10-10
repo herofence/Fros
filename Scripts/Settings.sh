b03c7df2612eacca1cc4c111c4505ae29a9827f0
herofence <113574064+herofence@users.noreply.github.com>
2023-10-10 18:51:15 +0800

    Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #删除dnsmasq插件
 #rm -rf -v $(find ./ -name "dnsmasq*")
 #rm -rf -v $(find ./ -type d 'dnsmasq*')
@@ -13,3 +14,5 @@ sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+#修改默认时间
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
